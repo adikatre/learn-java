79ac33a1c3b4719dd31026bfccf0406d2958b038
adikatre <adityad39c@gmail.com>
2025-12-27 20:50:37 -0800

    done with competitor!

--- frq/Competitor.java
+++ frq/Competitor.java
@@ -1,8 +1,9 @@
+
 public Round(String[] names) {
+    competitorList = new ArrayList<Competitor>();
     for (int i = 0; i < names.length(); i++) {
-        int rank = i + 1
-        competitorList.add(new Competitor(names[i], rank))
-	competitorList = new ArrayList<Competitor>();
+        int rank = i + 1;
+        competitorList.add(new Competitor(names[i], rank));
     }
 }
 
@@ -10,7 +11,7 @@ public Round(String[] names) {
 public ArrayList<Match> buildMatches() {
     int size = competitorList1.size();
     ArrayList<Match> matchesList = new ArrayList<Match>();
-    if (competitorList.size() % 2).equals(0) {
+    if (competitorList.size() % 2 == 0) {
         //even
         for (int i = 0; i < (competitorList.size()/2); i++) {
             Competitor competitor = competitorList.get(i);
@@ -23,23 +24,21 @@ public ArrayList<Match> buildMatches() {
     }
     else {
         //odd
-        ArrayList<Match> competitorList1 = new ArrayList<Match>;
+    ArrayList<Match> competitorList1 = new ArrayList<Match>();
 
 	// create competitorlist1
 	
-	for (int i = 0; i < competitorList.size();, i++) {
-	competitorlist1.add(competitorList.get(i));
+for (int i = 0; i < competitorList.size(); i++) {
+	competitorList1.add(competitorList.get(i));
 }
 
         competitorList1.remove(0);
-	int size = competitorList1.size();
+        int size = competitorList.size();
         for (int i = 0; i < (competitorList1.size()/2); i++) {
             Competitor competitor = competitorList1.get(i);
             int worstIndex = size - 1 - i;
-
             Match m = new Match(competitor, competitor.get(worstIndex));
-
             matchesList.add(m);
-
     }
+return matchesList;
 }
